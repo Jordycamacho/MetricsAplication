403330c69893f7a55f1acdd71032b5dc2d886cbb
Jordy Camacho <jordycamacho225@gmail.com>
2025-06-28 17:58:28 +0200

    git commit -m "feat(config): añade convertidor de json

---
+++ BackEnd/backend/src/main/java/com/fitapp/backend/infrastructure/config/HashMapConverter.java
@@ -0,0 +1,32 @@
+package com.fitapp.backend.infrastructure.config;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Converter
+public class HashMapConverter implements AttributeConverter<Map<String, Object>, String> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> attribute) {
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error converting map to JSON", e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String dbData) {
+        try {
+            return objectMapper.readValue(dbData, new TypeReference<Map<String, Object>>() {});
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error converting JSON to map", e);
+        }
+    }
+}
