8eed7015fa89db99f05b5d70228440484ac77690
Jordy Camacho <jordycamacho225@gmail.com>
2025-06-28 17:59:17 +0200

    git commit -m "feat(service): convertidor de tiempo en duracion por rango

---
+++ BackEnd/backend/src/main/java/com/fitapp/backend/domain/service/DurationToLongConverter.java
@@ -0,0 +1,19 @@
+package com.fitapp.backend.domain.service;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.time.Duration;
+
+@Converter(autoApply = true)
+public class DurationToLongConverter implements AttributeConverter<Duration, Long> {
+    @Override
+    public Long convertToDatabaseColumn(Duration duration) {
+        return duration != null ? duration.toSeconds() : null;
+    }
+
+    @Override
+    public Duration convertToEntityAttribute(Long dbData) {
+        return dbData != null ? Duration.ofSeconds(dbData) : null;
+    }
+}
+
