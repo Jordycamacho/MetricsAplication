0d1623df7b4ecb1139321291ebbc5f88324a43fc
Jordy Camacho <jordycamacho225@gmail.com>
2025-12-05 15:28:23 +0100

    excepcion global

---
+++ BackEnd/backend/src/main/java/com/fitapp/backend/domain/exception/DomainException.java
@@ -0,0 +1,20 @@
+package com.fitapp.backend.domain.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class DomainException extends RuntimeException {
+    private final String errorCode;
+    private final Map<String, Object> context;
+    
+    public DomainException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+        this.context = new HashMap<>();
+    }
+    
+    public DomainException withContext(String key, Object value) {
+        this.context.put(key, value);
+        return this;
+    }
+}
