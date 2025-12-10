79d576cacc6c03c335ce1fe29647dd06d4682f05
Jordy Camacho <jordycamacho225@gmail.com>
2025-12-10 22:04:48 +0100

    feat: dto para paginacion

---
+++ BackEnd/backend/src/main/java/com/fitapp/backend/application/dto/page/PageResponse.java
@@ -0,0 +1,21 @@
+package com.fitapp.backend.application.dto.page;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageResponse<T> {
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean first;
+    private boolean last;
+}
