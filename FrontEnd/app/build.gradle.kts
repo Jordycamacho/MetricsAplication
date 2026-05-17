675c1370bc8bd5d11adb5b89bb71e0c3ed124b8a
Jordy Camacho <jordycamacho225@gmail.com>
2026-05-17 23:03:10 +0200

    fix: Update API_BASE_URL in build.gradle.kts for development and local environments; remove unused buttons in fragment_create_package.xml and fragment_edit_package.xml; refactor activity_main.xml layout to use FrameLayout; add item_workout_day.xml and item_workout_exercise.xml for workout day and exercise representation; update bottom_nav_menu.xml to comment out marketplace item

--- FrontEnd/app/build.gradle.kts
+++ FrontEnd/app/build.gradle.kts
@@ -50,13 +50,13 @@ android {
             dimension = "environment"
             applicationIdSuffix = ".dev"
             versionNameSuffix = "-dev"
-            buildConfigField("String", "API_BASE_URL", "\"http://192.168.1.14:8080/\"")
+            buildConfigField("String", "API_BASE_URL", "\"http://192.168.1.15:8080/\"")
         }
         create("local") {
             dimension = "environment"
             applicationIdSuffix = ".local"
             versionNameSuffix = "-local"
-            buildConfigField("String", "API_BASE_URL", "\"http://192.168.1.14:8082/\"")
+            buildConfigField("String", "API_BASE_URL", "\"http://192.168.1.15:8082/\"")
         }
         create("prod") {
             dimension = "environment"
